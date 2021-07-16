f91fc6f3573b81a468d8799b6d45b54e7f681a8f
Antonio Andrade <antoniomaresmares001gmail.com>
2021-07-16 00:24:07 -0500

    primer version

---
+++ MiPrimerApi/MiPrimerApi/Models/Beer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MiPrimerApi.Models
+{
+    public partial class Beer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
